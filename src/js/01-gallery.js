15b2ee844f15738847b648d5619fde114407b457
kbilyi <71833120+kbilyi@users.noreply.github.com>
2022-02-16 21:38:00 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,42 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const gallaryEl = document.querySelector(".gallery");
+const createImageMarkupList = createImageMarkup(galleryItems);
+
+function createImageMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src='${preview}'
+          data-source='${original}'
+          alt='${description}'
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+gallaryEl.insertAdjacentHTML("afterbegin", createImageMarkupList);
+gallaryEl.addEventListener("click", onGallaryClick);
+
+function onGallaryClick(event) {
+  event.preventDefault();
+  const item = event.target.dataset.source;
+  const instanse = basicLightbox.create(`
+  <img src ="${item}" width="800" height="600">
+  `);
+  instanse.show();
+}
